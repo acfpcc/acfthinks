3c0e55035fcb9d01643b7deb6efb4f3e85d1a4b5
Paul Chun <me@paulchun.com>
2016-09-30 15:24:43 -0400

    Fix minor bugs

--- src/scripts/prompt.js
+++ src/scripts/prompt.js
@@ -34,59 +34,55 @@ function getToggleContent(width, height) {
   return container;
 }
 
-thoughtSubmittedToday(function(hasSubmitted) {
-  if (!hasSubmitted) {
-    var templateUrl = chrome.extension.getURL("templates/prompt.html");
-    $.get(templateUrl, {}, function(template) {
-      var render = _.template(template);
-      var data = {};
-      $('body').attr('id', 'acfthinks-body').html(render(data));
+var templateUrl = chrome.extension.getURL("templates/prompt.html");
+$.get(templateUrl, {}, function(template) {
+  var render = _.template(template);
+  var data = {};
+  $('body').attr('id', 'acfthinks-body').html(render(data));
 
-      $('.hi').click(function(e) {
+  $('.hi').click(function(e) {
+    e.preventDefault();
+    if ($(this).data("toggleContent") === undefined) {
+      $(this).data("toggleContent", 
+          getToggleContent($(this).width(), $(this).height()));
+    }
+    var newToggleContent = $(this).html();
+    $(this).html($(this).data("toggleContent"));
+    $(this).data("toggleContent", newToggleContent);
+  });
+
+  getLatestMessage(function(message) {
+    $('#latest-message').append("<p>"+str2Html(message.body)+"</p>");
+
+    $("#thought-bar").keypress(function (e) {
+      var key = e.which;
+      if (key == 13 && !e.shiftKey) {
         e.preventDefault();
-        if ($(this).data("toggleContent") === undefined) {
-          $(this).data("toggleContent", 
-              getToggleContent($(this).width(), $(this).height()));
-        }
-        var newToggleContent = $(this).html();
-        $(this).html($(this).data("toggleContent"));
-        $(this).data("toggleContent", newToggleContent);
-      });
-
-      getLatestMessage(function(message) {
-        $('#latest-message').append("<p>"+str2Html(message.body)+"</p>");
-
-        $("#thought-bar").keypress(function (e) {
-          var key = e.which;
-          if (key == 13 && !e.shiftKey) {
-            e.preventDefault();
-            $(this).attr('readonly', true).addClass("submitted");
-            var thought = $(this)[0].value;
-            saveThought(thought, message.url);
-            startCompletedTransition();
-          }
-        });
-
-        $(document)
-        .one('focus.textarea', '#thought-bar', function(){
-          var savedValue = this.value;
-          this.value = '';
-          this.baseScrollHeight = this.scrollHeight;
-          this.value = savedValue;
-        })
-        .on('input.textarea', '#thought-bar', function(){
-          var minRows = this.getAttribute('data-min-rows')|0;
-          var maxRows = this.getAttribute('data-max-rows');
-          if (maxRows == null) {
-            maxRows = 99999;
-          }
-          var rows;
-          this.rows = minRows;
-          rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 50);
-          this.rows = Math.min(minRows + rows, maxRows);
-        });
-
-      });
+        $(this).attr('readonly', true).addClass("submitted");
+        var thought = $(this)[0].value;
+        saveThought(thought, message.url);
+        startCompletedTransition();
+      }
     });
-  }
+
+    $(document)
+    .one('focus.textarea', '#thought-bar', function(){
+      var savedValue = this.value;
+      this.value = '';
+      this.baseScrollHeight = this.scrollHeight;
+      this.value = savedValue;
+    })
+    .on('input.textarea', '#thought-bar', function(){
+      var minRows = this.getAttribute('data-min-rows')|0;
+      var maxRows = this.getAttribute('data-max-rows');
+      if (maxRows == null) {
+        maxRows = 99999;
+      }
+      var rows;
+      this.rows = minRows;
+      rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 50);
+      this.rows = Math.min(minRows + rows, maxRows);
+    });
+
+  });
 });
